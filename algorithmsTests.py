8295d38159d22f1972b590ce68e8f0dad6b664b6
Adrian Szymanowski <adriano100417@gmail.com>
2021-05-08 21:06:29 +0200

    Refactored test cases to one method

--- algorithmsTests.py
+++ algorithmsTests.py
@@ -1,50 +1,45 @@
 import unittest
 
 import random
+from dataclasses import dataclass
+from typing import List
+
 from models import Demand, Path
 from algorithms import createGene, Gene
 
+
+class TestCase:
+  demand: Demand
+  expectedResult: List[int]
+
+  def __init__(self, volume, numberOfPaths, result):
+    paths = [Path([0])] * numberOfPaths
+    self.demand = Demand(0, 0, volume, paths)
+    self.expectedResult = result
+
+
 class DAPAlgorithmTests(unittest.TestCase):
 
-  def test_createGene_withValuesThatEqualsToSumOfDemandVolume(self):
-    volume = 4
-    paths = self.__getPaths(3)
-    demand = Demand(0, 0, volume, paths)
-    
-    self.__interceptSeed()
-    gene = createGene(demand)
+  def test_createGene_withValuesGeneratedSumToDemandVolume(self):
+    testCases = [
+      TestCase(volume=4, numberOfPaths=3, result=[3, 0, 1]),
+      TestCase(volume=2, numberOfPaths=3, result=[1, 0, 1]),
+      TestCase(volume=1, numberOfPaths=3, result=[1, 0, 0])
+    ]
 
-    self.assertEqual(gene.values, [3, 0, 1])
+    for case in testCases:
+      self.__interceptSeed()
+      result = createGene(case.demand)
+      self.assertEqual(result.values, case.expectedResult)
 
-
-  def test_createGene_withValuesThatEqualsToSumOfDemandVolume_SecondCase(self):
-    volume = 2
-    paths = self.__getPaths(3)
-    demand = Demand(0, 0, volume, paths)
-    
-    self.__interceptSeed()
-    gene = createGene(demand)
-
-    self.assertEqual(gene.values, [1, 0, 1])
-
-
-  def test_createGene_withValuesThatEqualsToSumOfDemandVolume_ThirdCase(self):
-    volume = 1
-    paths = self.__getPaths(3)
-    demand = Demand(0, 0, volume, paths)
-    
-    self.__interceptSeed()
-    gene = createGene(demand)
-
-    self.assertEqual(gene.values, [1, 0, 0])
 
   # MARK: - Helper methods
 
   def __interceptSeed(self):
     random.seed(2000)
 
-  def __getPaths(self, numberOfPaths):
-    return [Path([0])] * numberOfPaths 
+
+# MARK: - Launch 
 
 if __name__ == '__main__':
   unittest.main()
